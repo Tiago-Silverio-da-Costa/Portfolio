907c9d74f03fd0b16b17f41f6f2cc197aef28ca3
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2022-12-23 18:05:43 -0300

    fix

---
+++ backendSanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '4prevc6g',
+    dataset: 'production'
+  }
+})
---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo"/>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
